d2b90ba9fee93f717bb4044f817e563869d9f02e
Jannik Zschiesche <hello@apfelbox.net>
2012-09-19 20:48:23 +0200

    Updated and simplified the admin template

--- templates/admin_settings.php
+++ templates/admin_settings.php
@@ -1,61 +1,69 @@
 <?php
 /**
  * Admin page template
+ *
+ * @var $availableThemes string[]
+ * @var $theme string the current theme
+ * @var $availableExtensions Prism_Detached_Extension_Base[]
  */
 ?>
-<div class="icon32" id="icon-plugins"><br></div>
-<h2>Prism Detached</h2>
+<div class="wrap">
 
-<h2 style="margin-top: 40px;">Supported languages</h2>
-
-<ul class="ul-disc">
-    <li><code>css</code></li>
-    <li><code>java</code></li>
-    <li><code>javascript</code></li>
-    <li><code>markup</code></li>
-</ul>
-
-<h2 style="margin-top: 40px;">Settings</h2>
-<form action="<?php echo $pageUrl; ?>" method="post">
-    <table class="form-table">
-        <tbody>
-
-            <tr>
-                <th scope="row">
-                    <label for="settings-invisibles">Show Invisibles</label>
-                </th>
-                <td>
-                    <input type="checkbox" value="1" name="settings[invisibles]" id="settings-invisibles" <?php if ($invisibles) : ?>checked="checked"<?php endif; ?> />
-                </td>
-            </tr>
-
-            <tr>
-                <th scope="row">
-                    <label for="settings-autolinker">Use Autolinker</label>
-                </th>
-                <td>
-                    <input type="checkbox" value="1" name="settings[autolinker]" id="settings-autolinker" <?php if ($autolinker) : ?>checked="checked"<?php endif; ?> />
-                </td>
-            </tr>
-
-            <tr>
-                <th scope="row">
-                    <label for="settings-theme">Theme</label>
-                </th>
-                <td>
-                    <select name="settings[theme]" id="settings-theme">
-                        <?php foreach ($availableThemes as $key => $themeName) : ?>
-                        <option value="<?php echo esc_attr($key); ?>"<?php if ($key == $theme) : ?> selected="selected"<?php endif; ?>><?php echo esc_html($themeName); ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </td>
-            </tr>
+    <div class="icon32" id="icon-plugins"></div>
+    <h2 style="margin-bottom: 20px;">Prism Detached</h2>
 
 
-        </tbody>
-    </table>
 
-    <p class="submit">
-        <input type="submit" value="Save Settings" class="button-primary" id="submit" name="settings[submit]">
-    </p>
-</form>
+    <h3 style="margin-top: 40px;">Prism Core Supported languages</h3>
+    <ul class="ul-disc">
+        <li><code>css</code></li>
+        <li><code>java</code></li>
+        <li><code>javascript</code></li>
+        <li><code>markup</code></li>
+    </ul>
+
+
+
+    <h3 style="margin-top: 40px;">Theme</h3>
+    <form action="options.php" method="post">
+        <?php settings_fields('prism_detached_theme'); ?>
+        <p>
+            <select name="prism_detached_theme" id="settings-theme">
+                <?php foreach ($availableThemes as $key => $themeName) : ?>
+                <option value="<?php echo esc_attr($key); ?>"<?php if ($key == $theme) : ?> selected="selected"<?php endif; ?>><?php echo esc_html($themeName); ?></option>
+                <?php endforeach; ?>
+            </select>
+
+            <input type="submit" value="Save Theme" class="button-primary" />
+        </p>
+    </form>
+
+
+
+    <?php if (!empty($availableExtensions)) : ?>
+    <h3 style="margin-top: 40px;">Extensions</h3>
+    <form action="options.php" method="post">
+        <?php settings_fields('prism_detached_extensions'); ?>
+        <table class="form-table">
+            <tbody>
+
+                <?php foreach ($availableExtensions as $extension) : ?>
+                <tr>
+                    <th scope="row">
+                        <label for="extension-<?php echo esc_attr($extension->getId()); ?>"><?php echo esc_html($extension->getName()); ?></label>
+                    </th>
+                    <td>
+                        <input type="checkbox" name="prism_detached_extensions[<?php echo esc_attr($extension->getId()); ?>]" id="extension-<?php echo esc_attr($extension->getId()); ?>" value="1"<?php if ($extension->isActive()) : ?> checked="checked"<?php endif; ?> />
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+
+            </tbody>
+        </table>
+
+        <p class="submit">
+            <input type="submit" value="Save Extensions" class="button-primary" />
+        </p>
+    </form>
+    <?php endif; ?>
+</div>
